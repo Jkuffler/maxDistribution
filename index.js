3c75656c4ca75f9b5039e0ea00a627e7abc72c1c
Jason Kuffler <84805346+Jkuffler@users.noreply.github.com>
2022-08-29 22:19:41 +0000

    corrected math - just run the function in console and your distribution will max out with given MCN variables :)

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 
 function distributeMostCandy () {
-  let M = 3; // ttl kids
-  let N = 7; // ttl bags candy
-  let C = [1, 2, 3, 4, 5, 6, 7];// candy dump
+  const M = 3; // ttl kids
+  const C = [1, 2, 3, 4, 5, 6, 7];// candy dump
+  const N = C.length
+  // console.log(N)
   let totalPieces=0;
 
-  for(let i=0; i < C.length; i++) {
+  for(let i = 0; i < N; i++) {
      totalPieces += C[i];    
   };
-  let bagMax = totalPieces/M;
-  Math.floor(bagMax)
-  console.log(bagMax)
-  let remainingCandyPieces = totalPieces - (M*N);
+   // console.log(totalPieces)
+  const bagMax = Math.floor(totalPieces/M)
+  // console.log(bagMax)
+  const remainingCandyPieces = (totalPieces - (M*bagMax));
   return `Case# x: ${remainingCandyPieces}`
 };
